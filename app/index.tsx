593fe788500252d928f7d9f974487221fde3a7ca
edwardna1 <77164264+edwardna1@users.noreply.github.com>
2023-04-21 22:00:58 -0400

    added some anim

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,29 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Image, Text, View } from "react-native";
+import { Image, Text, View, Animated, Easing } from "react-native";
 import { Link } from "expo-router";
 import { useStore } from "@lib/store";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {
   useFonts,
   Lato_400Regular,
   OpenSans_400Regular,
   Inter_900Black,
-  NunitoSans_700Bold
-} from '@expo-google-fonts/dev';
+  NunitoSans_700Bold,
+} from "@expo-google-fonts/dev";
+import Breathing from "@components/Breathing";
+import { Logs } from "expo";
+import Animtitle from "@components/Animtitle";
+
+Logs.enableExpoCliLogging();
+console.log("LOGGING EXPO IOS CONSOLE");
 const App = () => {
+  // const fadeAnim = useRef(new Animated.Value(0)).current;
   const click = useStore(useCallback((state) => state.click, []));
   let [fontsLoaded] = useFonts({
-    'Inter': Inter_900Black,
-    'Sans' : OpenSans_400Regular,
-    'Lato' : Lato_400Regular,
-    'Nunlg' : NunitoSans_700Bold
+    Inter: Inter_900Black,
+    Sans: OpenSans_400Regular,
+    Lato: Lato_400Regular,
+    Nunlg: NunitoSans_700Bold,
   });
 
   if (!fontsLoaded) {
@@ -31,18 +38,28 @@ const App = () => {
         />
         <Text>Photo by Rumman Amin on Unsplash</Text>
       </View> */}
-      <Text className="text-7xl font-bold text-white font-nunl">Watch</Text>
-      <Link
-        className="text-center w-full text-2xl p-2 text-white font-nunl"
-        href="/hello"
-      >
-        Tap to get started
-      </Link>
+      <Animtitle>
+        <Image
+          className="ml-5"
+          source={require("@assets/Add_round_light.png")}
+        />
+        {/* <Text className="text-7xl font-bold text-white font-nunl tracking-widest"> */}
+          {/* Watch
+        </Text> */}
+      </Animtitle>
+      <View>
+        <Link
+          className={`text-center w-full text-2xl p-2 text-white font-nunl`}
+          href="/hello"
+        >
+          <Breathing>
+            <Text className="text-white text-2xl">Tap to get started</Text>
+          </Breathing>
+        </Link>
+      </View>
+      <View></View>
       <Text className="text-xl">Look at the number of clicks: {click}</Text>
-      <Link
-        className="text-center w-48 text-white text-md p-2"
-        href="/clicks"
-      >
+      <Link className="text-center w-48 text-white text-md p-2" href="/clicks">
         Go to the testing page
       </Link>
     </SafeAreaView>
---
+++ components/Animtitle.tsx
@@ -0,0 +1,79 @@
+import React, { PropsWithChildren, useEffect, useRef, useState } from "react";
+import { Animated, ViewStyle, Text, Easing } from "react-native";
+
+type BreathingProps = PropsWithChildren<{
+  style?: ViewStyle;
+  isText?: boolean;
+  transform?: boolean;
+}>;
+
+const Animtitle: React.FC<BreathingProps> = (props) => {
+  const fadeAnim = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
+
+  const size = fadeAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 10],
+  });
+
+  const startAnimation = () => {
+    Animated.sequence([]).start();
+  };
+
+  useEffect(() => {
+    console.log("here1");
+
+    Animated.sequence([
+      // Animated.delay(2000),
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(fadeAnim, {
+            toValue: 1,
+            duration: 1000,
+            easing: Easing.linear,
+            useNativeDriver: true,
+          }),
+          Animated.timing(fadeAnim, {
+            toValue: 0,
+            duration: 1000,
+            easing: Easing.linear,
+            useNativeDriver: true,
+          }),
+        ])
+      ),
+    ]).start();
+  }, [fadeAnim]);
+
+  return props.isText ? (
+    <Animated.Text // Special animatable View
+      style={{
+        ...props.style,
+        opacity: fadeAnim, // Bind opacity to animated value
+      }}
+    >
+      {props.children}
+    </Animated.Text>
+  ) : (
+    <Animated.View // Special animatable View
+      style={[
+        {
+          ...props.style,
+          opacity: fadeAnim, // Bind opacity to animated value
+        },
+        {
+          transform: [
+            {
+              scaleX: fadeAnim,
+            },
+            {
+              scaleY: fadeAnim,
+            },
+          ],
+        },
+      ]}
+    >
+      {props.children}
+    </Animated.View>
+  );
+};
+
+export default Animtitle;
